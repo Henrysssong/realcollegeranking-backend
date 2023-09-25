62414af0dddaaaa8de7ec3fe7aafca3cd4f2e9c4
Henry Song <feidong2002@berkeley.edu>
2023-09-24 22:58:57 -0700

    modified again

--- routes/rankings.js
+++ routes/rankings.js
@@ -1,7 +1,19 @@
+const axios = require('axios'); // Make sure to install axios: npm install axios
+
 router.get('/generate-ranking', async (req, res) => {
     const userAnswers = req.user.answers;
-    // Communicate with ChatGPT API using userAnswers to generate a personalized ranking
-    // For simplicity, we're skipping the actual API call here
-    const ranking = "Sample Ranking";  // Placeholder
-    res.render('result', { ranking: ranking });
+    
+    try {
+        const response = await axios.post('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', {
+            input: userAnswers // Modify this based on how you want to structure the input
+        });
+        
+        const ranking = response.data.output; // Modify based on the API response structure
+        res.render('result', { ranking: ranking });
+    } catch (error) {
+        console.error("Error generating ranking:", error);
+        res.status(500).send("Error generating ranking");
+    }
 });
+
+
